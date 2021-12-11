9d58c43f819a218281723d43a118cabe0ae7d823
LosSantosSavior <nickrussell001@gmail.com>
2021-12-11 17:55:29 -0500

    my javafx sdk finally f***ing works - Nick, 5:56PM 12/11/21

--- src/main/java/com/example/project5/GDPController.java
+++ src/main/java/com/example/project5/GDPController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.fxml.Initializable;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -12,6 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class GDPController {
+public class GDPController implements Initializable {
 
+    public void loadData()  {
+        var site = "";
+    }
+
+    public void loadList()  {
+
+    }
+
+    public void yearChanged()   {
+
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
